94640b62e3424e4dcb556da879a4e20fa561227e
ToyGamer <ToyGamer@users.noreply.github.com>
2018-11-27 01:41:42 -0600

    Running In the 90's
    And then there was moonwalks

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed;
+
+    private Animator animator;
+
+    protected Vector2 direction;
+
+    // Use this for initialization
+    protected virtual void Start ()
+    {
+        speed = 5;
+        animator = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Move();
+    }
+    /// <summary>
+    /// Moves the Player
+    /// </summary>
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        AnimateMovement(direction);
+    }
+
+    public void AnimateMovement(Vector2 direction)
+    {
+        animator.SetFloat("x", direction.x);
+        animator.SetFloat("y", direction.y);
+    }
+    
+}
